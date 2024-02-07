2861fc5eaa530e9509bd4a66cb61ec12610bf138
Steenos <steeeenos@gmail.com>
2024-02-07 15:27:51 -0500

    added argparse functionality

--- exercise3_2.py
+++ exercise3_2.py
@@ -1,38 +1,90 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# 1. a)  Make a plot of the deltoid curve
-#  x = 2 cos θ + cos 2θ, y = 2 sin θ − sin 2θ, where 0 ≤ θ < 2π
-theta = np.linspace(0, 2*np.pi, 1000)
+import argparse
 
-x = 2*np.cos(theta) + np.cos(2*theta)
-y = 2*np.sin(theta) - np.sin(2*theta)
+def deltoid():
+    theta = np.linspace(0, 2*np.pi, 1000)
 
-plt.title("Deltoid curve")
-plt.plot(x, y, label='x = 2 cos θ + cos 2θ, y = 2 sin θ - sin 2θ')
-plt.legend()
-plt.show()
+    x = 2*np.cos(theta) + np.cos(2*theta)
+    y = 2*np.sin(theta) - np.sin(2*theta)
 
-# 2. b) make a plot of the Galilean spiral, r=θ2 for 0 ≤ θ ≤ 10π.
-theta = np.linspace(0, 10*np.pi, 1000)
-r = theta**2
+    return x,y,theta
+def galilean_spiral():
+    theta = np.linspace(0, 10*np.pi, 1000)
+    r = theta**2
 
-x = r*np.cos(theta)
-y = r*np.sin(theta)
+    x = r*np.cos(theta)
+    y = r*np.sin(theta)
 
-plt.title("Galilean spiral")
-plt.plot(x, y, label='r=θ^2')
-plt.legend()
-plt.show()
+    return x,y,r,theta
 
-# 3. c)  Using the same method, make a polar plot of “Fey’s function”
-theta = np.linspace(0, 24*np.pi, 1000)
-r = np.exp(np.cos(theta)) - 2 * np.cos(4*theta) + np.sin(theta/12)**5
-x = r*np.cos(theta)
-y = r*np.sin(theta)
-plt.title("Fey's function")
+def feys_function():
+    theta = np.linspace(0, 24*np.pi, 1000)
+    r = np.exp(np.cos(theta)) - 2 * np.cos(4*theta) + np.sin(theta/12)**5
+    x = r*np.cos(theta)
+    y = r*np.sin(theta)
+    
+    return x,y,r,theta
 
-plt.plot(x,y, label="r = exp(cos(θ)) - 2cos(4θ) + sin(θ/12)^5")
-plt.legend()
-plt.show()
+parser = argparse.ArgumentParser(
+                    prog='Visualize curves',
+                    description='Visualize the deltoid, galilean spiral and feys function',
+                    )
+parser.add_argument('-sp', '--subplots', help='Make subplots of the three functions',
+                    action='store_true') 
 
+parser.add_argument('-graph_type', choices=['deltoid', 'galilean', 'feys'], help='Choose which graph to plot')
 
+args = parser.parse_args()
+
+def main():
+    if args.subplots:
+        fig, ax = plt.subplots(3, 1, figsize=(10, 10))
+        x1, y1, theta1 = deltoid()
+        x2,y2,r2,theta2 = galilean_spiral()
+        x3,y3,r3,theta3 = feys_function()
+    
+        ax[0].plot(x1, y1, label='x = 2 cos θ + cos 2θ, y = 2 sin θ - sin 2θ')
+        ax[0].set_title("Deltoid curve")
+        ax[0].legend()
+
+        ax[1].plot(x2, y2, label='r=θ^2')
+        ax[1].set_title("Galilean spiral")
+        ax[1].legend()
+
+        ax[2].plot(x3,y3, label="r = exp(cos(θ)) - 2cos(4θ) + sin(θ/12)^5")
+        ax[2].set_title("Fey's function")
+        ax[2].legend()
+
+        plt.show()
+    elif args.graph_type == 'deltoid':
+        x1, y1, theta1 = deltoid()
+        plt.plot(x1, y1, label='x = 2 cos θ + cos 2θ, y = 2 sin θ - sin 2θ')
+        plt.legend()
+        plt.show()
+    elif args.graph_type == 'galilean':
+        x2,y2,r2,theta2 = galilean_spiral()
+        plt.plot(x2, y2, label='r=θ^2')
+        plt.legend()
+        plt.show()
+    elif args.graph_type == 'feys':
+        x3,y3,r3,theta3 = feys_function()
+        plt.plot(x3,y3, label="r = exp(cos(θ)) - 2cos(4θ) + sin(θ/12)^5")
+        plt.legend()
+        plt.show()
+    else:
+        x1, y1, theta1 = deltoid()
+        x2,y2,r2,theta2 = galilean_spiral()
+        x3,y3,r3,theta3 = feys_function()
+        plt.plot(x1, y1, label='x = 2 cos θ + cos 2θ, y = 2 sin θ - sin 2θ')
+        plt.legend()
+        plt.show()
+        plt.plot(x2, y2, label='r=θ^2')
+        plt.legend()
+        plt.show()
+        plt.plot(x3,y3, label="r = exp(cos(θ)) - 2cos(4θ) + sin(θ/12)^5")
+        plt.legend()
+        plt.show()
+
+if __name__ == '__main__':
+    main()
